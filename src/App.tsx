9307cb9e4a9b3e6a1f61a4b4cc965103a6a8d347
TheDrakl <denysmelnyk262626@gmail.com>
2025-06-03 21:50:53 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-bgDark min-h-[200vh]">Hi!</div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgDark: '#1C1C22',
+        lightGreen: '#22c55e',
+      },
+    },
+  },
+  plugins: [],
+}
